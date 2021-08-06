5350c49d8e9db7648e9d0d26a889feb85de9fb36
uudaykuma <uudaykuma@gmail.com>
2021-08-07 00:36:58 +0530

    landing page

---
+++ front_end/src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import About from './About'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <About />
+        </div>
+    )
+}
+
+export default LandingPage
